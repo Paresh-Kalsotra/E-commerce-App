871812c6dc68b1dc363939e7fe29da045479d881
Paresh-Kalsotra <pareshkalsotra@gmail.com>
2024-03-30 04:53:35 +0530

    e commerce app upload

---
+++ frontend/src/components/OrderListItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const OrderListItem = ({ item }) => {
+  console.log(item);
+  return <div>OrderListItem</div>;
+};
+
+OrderListItem.propTypes = {};
+
+export default OrderListItem;
--- frontend/src/pages/OrderPage.jsx
+++ frontend/src/pages/OrderPage.jsx
@@ -16,7 +16,39 @@ const OrderPage = () => {
   }
 
   console.log(orders);
-  return <div>OrderPage</div>;
+  return (
+    <div style={{ border: "1px solid #378eeb", margin: "4% 8%" }}>
+      <h3
+        style={{
+          background: "#378eeb",
+          color: "#f0f0f0",
+          textAlign: "center",
+          padding: "10px",
+        }}
+      >
+        Your Orders
+      </h3>
+      <div className="orderList">
+        {orders && orders.length > 0 ? (
+          <div>
+            {orders.map((item) => (
+              <orderListItem key={item.id} item={item} />
+            ))}
+          </div>
+        ) : (
+          <div
+            style={{
+              textAlign: "center",
+              padding: "100px",
+            }}
+          >
+            <p>No orders yet.</p>
+            <p>Explore our products and start shopping!</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrderPage;
